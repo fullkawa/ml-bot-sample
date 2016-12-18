f077d759626c507f9e8553a16a245a13e0950833
y.furukawa <y.furukawa@lexues.co.jp>
2016-12-18 21:33:36 +0900

    コードを(ほぼ)元に戻しても実行できることを確認した。

--- train.py
+++ train.py
@@ -48,39 +48,34 @@ df["sin_t"] = df.t.apply(lambda x: math.sin(x * (2 * math.pi / steps_per_cycle)+
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation
 from keras.layers.recurrent import LSTM
-#import keras.backend.tensorflow_backend as KTF # 下記理由により TensorBoard を使わないので省略
+import keras.backend.tensorflow_backend as KTF
 import tensorflow as tf
-#from keras.callbacks import EarlyStopping, TensorBoard, ModelCheckpoint # keras0.3.0 では TensorBoard がなくなっていたので修正
-from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.callbacks import EarlyStopping, TensorBoard, ModelCheckpoint
 import os
 
 in_out_neurons = 1
 hidden_neurons = 300
 #length_of_sequences = 100 # 上へ移動
 
-#old_session = KTF.get_session()
+old_session = KTF.get_session()
 
 with tf.Graph().as_default():
-#     session = tf.Session('')
-#     KTF.set_session(session)
-#     KTF.set_learning_phase(1)
+    session = tf.Session('')
+    KTF.set_session(session)
+    KTF.set_learning_phase(1)
     model = Sequential()  
     with tf.name_scope("inference") as scope:
         model.add(LSTM(hidden_neurons, input_shape=(length_of_sequences, in_out_neurons), return_sequences=False))
         model.add(Dense(in_out_neurons))
         model.add(Activation("linear"))
-    #model.summary() # 'Sequential' object has no attribute 'summary' と怒られた
-    #fpath = './tensorlog/weights.{epoch:02d}-{loss:.2f}-{val_loss:.2f}.hdf5' # KeyError: 'loss' と怒られた
-    fpath = './weights.{epoch:02d}.hdf5'
+    model.summary()
+    fpath = './tensorlog/weights.{epoch:02d}-{loss:.2f}-{val_loss:.2f}.hdf5'
     cp_cb = ModelCheckpoint(filepath = fpath, monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
-    #es_cb = EarlyStopping(monitor='val_loss', patience=2, verbose=1, mode='auto') # __init__() got an unexpected keyword argument 'mode' と怒られた
-    es_cb = EarlyStopping(monitor='val_loss', patience=2, verbose=1)
-    #tb_cb = TensorBoard(log_dir="./tensorlog", histogram_freq=1)
-    #model.compile(loss="mean_squared_error", optimizer="rmsprop",  metrics=['accuracy']) # compile() got an unexpected keyword argument 'metrics' と怒られた
-    model.compile(loss="mean_squared_error", optimizer="rmsprop")
-    #model.fit(X_train, y_train, batch_size=600, nb_epoch=10, validation_split=0.05, verbose=1, callbacks=[cp_cb, es_cb, tb_cb])
-    model.fit(X_train, y_train, batch_size=600, nb_epoch=10, validation_split=0.05, verbose=1, callbacks=[cp_cb, es_cb])
+    es_cb = EarlyStopping(monitor='val_loss', patience=2, verbose=1, mode='auto')
+    tb_cb = TensorBoard(log_dir="./tensorlog", histogram_freq=1)
+    model.compile(loss="mean_squared_error", optimizer="rmsprop",  metrics=['accuracy'])
+    model.fit(X_train, y_train, batch_size=600, nb_epoch=10, validation_split=0.05, verbose=1, callbacks=[cp_cb, es_cb, tb_cb])
 json_string = model.to_json()
 #open(os.path.join(f_model,'./tensorlog/rnn_model.json'), 'w').write(json_string)
-open(os.path.join('./rnn_model.json'), 'w').write(json_string)
-#KTF.set_session(old_session)
+open(os.path.join('./tensorlog/rnn_model.json'), 'w').write(json_string)
+KTF.set_session(old_session)
