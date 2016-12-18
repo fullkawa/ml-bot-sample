6a6986bc84ac2cf70e1b639994c4120a0e38dbc8
y.furukawa <y.furukawa@lexues.co.jp>
2016-12-18 18:08:30 +0900

    実装に手こずっているため、いったん保留。

--- model/temperature.py
+++ model/temperature.py
@@ -1,36 +1,71 @@
 # -*- coding:utf-8 -*-
 
-from keras.models import Sequential
-from keras.layers import Dense, Activation
-from numpy as np
+from keras.models import Sequential, model_from_json
+from keras.layers.core import Dense, Activation
+from keras.layers.recurrent import LSTM
+import numpy as np
+import os
+import time
 
 class Temperature(object):
     OUTPUT_DIM = 32 #内部の射影と最終的な出力の次元
-    WEIGHTS_FILE = 'weights.hdf5'
+    MODEL_FILE = os.path.join('model', 'model.json')
+    WEIGHTS_FILE = os.path.join('model', 'weights.hdf5')
+    DT_FORMAT = '%H:%M:%S'
 
     def __init__(self):
-        self.model = Sequential([
-            LSTM(OUTPUT_DIM),
-            Dense(1),
-            Activation('linear')
-        ])
-        self.model.compile(loss='mean_squared_error', optimizer='rmsprop')
-
-    def train(self, X, y, batch_size):
+        self.model = None
+        
+    def train(self, X, y, dev=False):
         """モデルの学習を行い、その結果を保存する
         @param X: 学習データ(予測に使われるパラメータ＝月,日)
         @param y: 学習データ(予測したい値＝平均気温)
-        @param batch_size: 学習データのサイズ
         """
-        self.model.fit(X, y, batch_size=batch_size, validation_split=0.05)
-        self.model.save_weights(WEIGHTS_FILE)
+        if dev and os.path.exists(Temperature.MODEL_FILE):
+            model_json = open(Temperature.MODEL_FILE).read()
+            print 'Load model from', Temperature.MODEL_FILE
+            self.model = model_from_json(model_json)
+        else:
+            self.model = Sequential([
+                LSTM(Temperature.OUTPUT_DIM, input_shape=X.shape),
+                Dense(1),
+                Activation('linear')
+            ])
+            self.model.compile(loss='mean_squared_error', optimizer='rmsprop')
+            model_json = self.model.to_json()
+            print 'model_json:', model_json #DEBUG
+            print 'Save model to', Temperature.MODEL_FILE, 'at', time.strftime(Temperature.DT_FORMAT)
+            with open(Temperature.MODEL_FILE, 'w') as f:
+                f.write(model_json)
+                f.close()
+        
+        print 'Train started at', time.strftime(Temperature.DT_FORMAT)
+        self.model.fit(X, y, batch_size=128, validation_split=0.05)
+        print 'Train finished at', time.strftime(Temperature.DT_FORMAT)
+        
+        print 'Save weights to', Temperature.WEIGHTS_FILE
+        self.model.save_weights(Temperature.WEIGHTS_FILE)
 
     def load(self):
         """学習済みモデルを読み込む
         """
-        self.model.load_weights(WEIGHTS_FILE)
+        assert os.path.exists(Temperature.MODEL_FILE)
+        assert os.path.exists(Temperature.WEIGHTS_FILE)
+        
+        model_json = open(Temperature.MODEL_FILE).read()
+        print 'Load model from', Temperature.MODEL_FILE
+        self.model = model_from_json(model_json)
+        
+        print 'Load weights from', Temperature.WEIGHTS_FILE
+        self.model.load_weights(Temperature.WEIGHTS_FILE)
 
     def predict(self, month, day):
+        """モデルを使って平均気温を予測する
+        @param month: 月
+        @param day: 日
+        """
+        assert self.model is not None
+        
         X = np.array([[month, day]])
         print 'X:', X #DEBUG
         predicted = self.model.predict(X)
--- train/train.py
+++ train/train.py
@@ -1,9 +1,31 @@
 # -*- coding:utf-8 -*-
 
+# Usage: ml-bot-sample you$ python train/train.py
+import sys
+sys.path.append('.')
+
+import os
 import pandas as pd
+import matplotlib.pyplot as plt
+import time
 from model.temperature import Temperature
 
-DATA_FILE = 'data.csv'
+DATA_FILE = os.path.join('train', 'data.csv')
+DT_FORMAT = '%H:%M:%S'
 
-data = pd.DataFrame.from_csv(DATA_FILE, header=3)
+data = pd.DataFrame.from_csv(DATA_FILE, header=2, index_col=None, parse_dates=False)
+print '\n[data]'
 print data.head()
+
+X_train = data.iloc[:730, 1:3] #DEV
+print '\n[train data: X]', X_train.shape
+print X_train.head()
+
+y_train = data.iloc[:730, 3] #DEV
+print '\n[train data: y]', y_train.shape
+print y_train.head()
+
+temp = Temperature()
+print '\n*** train start at', time.strftime(DT_FORMAT)
+temp.train(X_train.values, y_train.values, dev=True)
+print '\n*** train finished at', time.strftime(DT_FORMAT)
