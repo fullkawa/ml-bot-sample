b8ab2c04a8ca8deabf3c1b18db6798441605a71f
y.furukawa <y.furukawa@lexues.co.jp>
2016-12-18 18:44:40 +0900

    先人のコードを拝借し、Keras0.3.0で動くようにした。

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # ml-bot-sample
-Sample bot using machine learning(keras)
+
+機械学習を使ったボットのサンプルプログラム
+
+## バージョン情報
+
+* Python 2.7
+* Tensorflow 0.10.0rc0
+* Keras 0.3.0
+
+## 概要
+
+yukiB氏の
+[Kerasのcallbackを試す（modelのsave,restore/TensorBoard書き出し/early stopping）](http://qiita.com/yukiB/items/f45f0f71bc9739830002)
+を利用し、RNN(LSTM)にsin波を学習させ、その値を返すボットという設定です。
+
+## 使い方
+
+### モデルにsin波を学習させる
+
+`git clone https://github.com/fullkawa/ml-bot-sample.git`でソースコードをチェックアウトした後、`python train.py`
+
+```
+112-233:ml-bot-sample y.furukawa$ python train.py
+Using TensorFlow backend.
+Train on 3325 samples, validate on 176 samples
+Epoch 1/10
+3325/3325 [==============================] - 27s - loss: 0.2050 - val_loss: 0.1024
+Epoch 00000: val_loss improved from inf to 0.10237, saving model to ./weights.00.hdf5
+Epoch 2/10
+3325/3325 [==============================] - 25s - loss: 0.0341 - val_loss: 0.0009
+Epoch 00001: val_loss improved from 0.10237 to 0.00093, saving model to ./weights.01.hdf5
+Epoch 3/10
+3325/3325 [==============================] - 26s - loss: 0.0009 - val_loss: 0.0009
+Epoch 00002: val_loss improved from 0.00093 to 0.00091, saving model to ./weights.02.hdf5
+Epoch 4/10
+3325/3325 [==============================] - 25s - loss: 0.0028 - val_loss: 0.0301
+Epoch 00003: val_loss did not improve
+Epoch 5/10
+3325/3325 [==============================] - 25s - loss: 0.0200 - val_loss: 0.0045
+Epoch 00004: val_loss did not improve
+Epoch 6/10
+3325/3325 [==============================] - 25s - loss: 0.0033 - val_loss: 0.0068
+Epoch 00005: val_loss did not improve
+Epoch 00005: early stopping
+112-233:ml-bot-sample y.furukawa$
+112-233:ml-bot-sample y.furukawa$ ls -al
+ (中略)
+-rw-r--r--   1 y.furukawa  222086365      829 12 18 17:55 rnn_model.json
+-rw-r--r--   1 y.furukawa  222086365     3687 12 18 17:52 train.py
+-rw-r--r--   1 y.furukawa  222086365  1460448 12 18 17:53 weights.00.hdf5
+-rw-r--r--   1 y.furukawa  222086365  1460448 12 18 17:54 weights.01.hdf5
+-rw-r--r--   1 y.furukawa  222086365  1460448 12 18 17:54 weights.02.hdf5
+```
+
+上記"rnn_model.json"がモデルデータ、"weights.*.hdf5"が学習済みパラメータデータとなります。
+
+
---
+++ train.py
@@ -0,0 +1,86 @@
+# -*- coding:utf-8 -*-
+
+# Kerasのcallbackを試す（modelのsave,restore/TensorBoard書き出し/early stopping）
+# @see http://qiita.com/yukiB/items/f45f0f71bc9739830002
+
+# Make data 
+import pandas as pd
+import numpy as np
+import math
+import random
+
+def _load_data(data, n_prev = 100):
+    """
+    data should be pd.DataFrame()
+    """
+
+    docX, docY = [], []
+    for i in range(len(data)-n_prev):
+        docX.append(data.iloc[i:i+n_prev].as_matrix())
+        docY.append(data.iloc[i+n_prev].as_matrix())
+    alsX = np.array(docX)
+    alsY = np.array(docY)
+
+    return alsX, alsY
+
+def train_test_split(df, test_size=0.1, n_prev = 100):
+    """
+    This just splits data to training and testing parts
+    """
+    ntrn = round(len(df) * (1 - test_size))
+    ntrn = int(ntrn)
+    X_train, y_train = _load_data(df.iloc[0:ntrn], n_prev)
+    X_test, y_test = _load_data(df.iloc[ntrn:], n_prev)
+
+    return (X_train, y_train), (X_test, y_test)
+
+random.seed(0)
+random_factor = 0.05
+steps_per_cycle = 80
+number_of_cycles = 50
+length_of_sequences = 100 # Add
+
+df = pd.DataFrame(np.arange(steps_per_cycle * number_of_cycles + 1), columns=["t"])
+df["sin_t"] = df.t.apply(lambda x: math.sin(x * (2 * math.pi / steps_per_cycle)+ random.uniform(-1.0, +1.0) * random_factor))
+(X_train, y_train), (X_test, y_test) = train_test_split(df[["sin_t"]], n_prev =length_of_sequences)
+
+# Train model
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
+from keras.layers.recurrent import LSTM
+#import keras.backend.tensorflow_backend as KTF # 下記理由により TensorBoard を使わないので省略
+import tensorflow as tf
+#from keras.callbacks import EarlyStopping, TensorBoard, ModelCheckpoint # keras0.3.0 では TensorBoard がなくなっていたので修正
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+import os
+
+in_out_neurons = 1
+hidden_neurons = 300
+#length_of_sequences = 100 # 上へ移動
+
+#old_session = KTF.get_session()
+
+with tf.Graph().as_default():
+#     session = tf.Session('')
+#     KTF.set_session(session)
+#     KTF.set_learning_phase(1)
+    model = Sequential()  
+    with tf.name_scope("inference") as scope:
+        model.add(LSTM(hidden_neurons, input_shape=(length_of_sequences, in_out_neurons), return_sequences=False))
+        model.add(Dense(in_out_neurons))
+        model.add(Activation("linear"))
+    #model.summary() # 'Sequential' object has no attribute 'summary' と怒られた
+    #fpath = './tensorlog/weights.{epoch:02d}-{loss:.2f}-{val_loss:.2f}.hdf5' # KeyError: 'loss' と怒られた
+    fpath = './weights.{epoch:02d}.hdf5'
+    cp_cb = ModelCheckpoint(filepath = fpath, monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
+    #es_cb = EarlyStopping(monitor='val_loss', patience=2, verbose=1, mode='auto') # __init__() got an unexpected keyword argument 'mode' と怒られた
+    es_cb = EarlyStopping(monitor='val_loss', patience=2, verbose=1)
+    #tb_cb = TensorBoard(log_dir="./tensorlog", histogram_freq=1)
+    #model.compile(loss="mean_squared_error", optimizer="rmsprop",  metrics=['accuracy']) # compile() got an unexpected keyword argument 'metrics' と怒られた
+    model.compile(loss="mean_squared_error", optimizer="rmsprop")
+    #model.fit(X_train, y_train, batch_size=600, nb_epoch=10, validation_split=0.05, verbose=1, callbacks=[cp_cb, es_cb, tb_cb])
+    model.fit(X_train, y_train, batch_size=600, nb_epoch=10, validation_split=0.05, verbose=1, callbacks=[cp_cb, es_cb])
+json_string = model.to_json()
+#open(os.path.join(f_model,'./tensorlog/rnn_model.json'), 'w').write(json_string)
+open(os.path.join('./rnn_model.json'), 'w').write(json_string)
+#KTF.set_session(old_session)
